43de99e29637d95883785d805a1396b968108a6d
ibaczewska <izabela.baczewska@gmail.com>
2019-06-27 22:30:31 +0200

    fixed

--- Cartoon/js/mainjs.js
+++ Cartoon/js/mainjs.js
@@ -2,7 +2,7 @@
 function playSound(e){
       const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
       
-      const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
+      const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
       
       if (!audio) return; //stop the function from running all together
       audio.currentTime = 0; //rewind to the start
@@ -13,12 +13,12 @@ function playSound(e){
  
 
   function removeTransition(e){
-    if(e.propertyName !== 'transform') return;
+    if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 
   }
 
   const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   
   window.addEventListener('keydown', playSound);
